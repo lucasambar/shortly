21292692853ff00de347a0bb281cac396fb896c9
Lucas <lucass.ambar@gmail.com>
2022-12-20 19:31:27 -0300

    feat: route GET urls/:id

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -12,3 +12,20 @@ export async function postShorten (req, res) {
         res.sendStatus(500)
     }
 }
+
+export async function getUrlById (req, res) {
+    const id = req.params.id
+
+    try {
+        const urlDB = await connectionDB.query('SELECT * FROM urls WHERE id=$1',[id])
+        if (urlDB.rowCount === 0) return res.sendStatus(404)
+
+        const url = urlDB.rows[0]
+        res.status(200).send({id, 
+            shortUrl:url.newLink, 
+            url:url.userLink})
+    } catch (erro) {
+        console.log(erro)
+        res.sendStatus(500)
+    }
+}
--- src/routes/urls.route.js
+++ src/routes/urls.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 
-import { postShorten } from "../controllers/urls.controller.js"
+import { getUrlById, postShorten } from "../controllers/urls.controller.js"
 import { validateUrl } from "../middlewares/urls.middleware.js"
 import { validateToken } from "../middlewares/users.middleware.js"
 
 const router = express.Router()
 
 router.post("/urls/shorten", validateToken, validateUrl, postShorten)
+router.get("/urls/:id", getUrlById)
 
 export default router
