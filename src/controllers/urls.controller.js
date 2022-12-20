59016c1de879d3cbfef0e42427ccc970d16d1897
Lucas <lucass.ambar@gmail.com>
2022-12-20 19:50:44 -0300

    feat: route GET  open/:shortenUrl

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -29,3 +29,23 @@ export async function getUrlById (req, res) {
         res.sendStatus(500)
     }
 }
+
+export async function redirectUrl (req, res) {
+    const short = req.params.shortUrl
+
+    try {
+        const urlDB = await connectionDB.query('SELECT * FROM urls WHERE "newLink"=$1',[short])
+        if (urlDB.rowCount == 0) return res.sendStatus(404)
+
+        const url = urlDB.rows[0]
+        url.views ++
+
+        await connectionDB.query('UPDATE urls SET views=$1 WHERE "newLink"=$2',
+        [url.views, short])
+
+        res.redirect(url.userLink)
+    } catch (erro) {
+        console.log(erro)
+        res.sendStatus(500)
+    }
+}
--- src/routes/urls.route.js
+++ src/routes/urls.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 
-import { getUrlById, postShorten } from "../controllers/urls.controller.js"
+import { getUrlById, postShorten, redirectUrl } from "../controllers/urls.controller.js"
 import { validateUrl } from "../middlewares/urls.middleware.js"
 import { validateToken } from "../middlewares/users.middleware.js"
 
@@ -8,5 +8,6 @@ const router = express.Router()
 
 router.post("/urls/shorten", validateToken, validateUrl, postShorten)
 router.get("/urls/:id", getUrlById)
+router.get("/urls/open/:shortUrl", redirectUrl)
 
 export default router
