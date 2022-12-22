4edb3b0de29413fc216195c0bd580f16407aa61e
Lucas <lucass.ambar@gmail.com>
2022-12-21 23:23:08 -0300

    refact: ranking route with repository pattern

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -1,19 +1,9 @@
 import {connectionDB} from "../databases/db.js"
+import { ranking } from "../repositories/ranking.repository.js"
 
 export async function getRanking (req, res) {
     try {
-        const {rows} = await connectionDB.query
-        (`SELECT 
-        urls."userId" AS id,
-        u.name,
-        COUNT(urls.id) AS "linksCount",
-        SUM(urls.views) AS "visitCount"
-        FROM urls 
-        JOIN users u ON u.id = urls."userId"
-        GROUP BY u.name, urls."userId"
-        ORDER BY u.name DESC 
-        LIMIT 10
-        `)
+        const {rows} = await ranking()
 
         res.send(rows)
     } catch (erro) {
